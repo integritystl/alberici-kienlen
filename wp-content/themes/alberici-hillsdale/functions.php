4908f611ee9a0bccad9811998bc1edc861468b7f
laura-khl <laura.kong@integritystl.com>
2018-08-07 15:40:24 -0500

    add SVG support function

--- wp-content/themes/alberici-hillsdale/functions.php
+++ wp-content/themes/alberici-hillsdale/functions.php
@@ -192,6 +192,15 @@ if ( defined( 'JETPACK__VERSION' ) ) {
 	require get_template_directory() . '/inc/jetpack.php';
 }
 
+//SVG Support in Media Uploader
+function add_file_types_to_uploads($file_types){
+	$new_filetypes = array();
+	$new_filetypes['svg'] = 'image/svg+xml';
+	$file_types = array_merge($file_types, $new_filetypes );
+	return $file_types;
+}
+add_action('upload_mimes', 'add_file_types_to_uploads');
+
 //Hide sections we don't use
 function remove_menus(){
   remove_menu_page( 'edit-comments.php' );          //Comments
