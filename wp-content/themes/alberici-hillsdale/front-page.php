be35b354928ef898a771a0cdcf6a5c379cad1f0c
laura-khl <laura.kong@integritystl.com>
2018-08-08 15:59:13 -0500

    display homepage hero area

--- wp-content/themes/alberici-hillsdale/front-page.php
+++ wp-content/themes/alberici-hillsdale/front-page.php
@@ -13,44 +13,26 @@
  */
 
 get_header();
+$HeroImage = get_field('homepage_hero_featured_image');
+$HeroHeadline = get_field('homepage_hero_title');
+$HeroContent = get_field('homepage_hero_content');
+$HeroButtonText = get_field('homepage_hero_button_text');
+$HeroButtonLink = get_field('homepage_hero_button_link');
 ?>
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main">
-			<h1>FRONTPAGE</h1>
-		<?php
-		if ( have_posts() ) :
-
-			if ( is_home() && ! is_front_page() ) :
-				?>
-				<header>
-					<h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
-				</header>
-				<?php
-			endif;
-
-			/* Start the Loop */
-			while ( have_posts() ) :
-				the_post();
-
-				/*
-				 * Include the Post-Type-specific template for the content.
-				 * If you want to override this in a child theme, then include a file
-				 * called content-___.php (where ___ is the Post Type name) and that will be used instead.
-				 */
-				get_template_part( 'template-parts/content', get_post_type() );
-
-			endwhile;
-
-			the_posts_navigation();
-
-		else :
-
-			get_template_part( 'template-parts/content', 'none' );
-
-		endif;
-		?>
-
+			<?php if ($HeroHeadline): ?>
+				<div class="homepage-hero hero-image" style="background-image: url(<?php if ($HeroImage): echo $HeroImage; endif; ?>);">
+					<div class="container">
+						<h1><?php echo $HeroHeadline; ?></h1>
+						<p><?php echo $HeroContent; ?></p>
+						<?php if ($HeroButtonText): ?>
+							<a href="<?php echo $HeroButtonLink; ?>"/><?php echo $HeroButtonText; ?> </a>
+						<?php endif; ?>
+					</div>
+				</div>
+			<?php endif; ?>
 		</main><!-- #main -->
 	</div><!-- #primary -->
 
