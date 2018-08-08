ab551dab0ba232d077681ff31e60888d09a53ca6
laura-khl <laura.kong@integritystl.com>
2018-08-08 12:05:13 -0500

    footer callout template part setup

--- wp-content/themes/alberici-hillsdale/front-page.php
+++ wp-content/themes/alberici-hillsdale/front-page.php
@@ -17,39 +17,8 @@ get_header();
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main">
-			<h1>FRONTPAGE</h1>
-		<?php
-		if ( have_posts() ) :
-
-			if ( is_home() && ! is_front_page() ) :
-				?>
-				<header>
-					<h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
-				</header>
-				<?php
-			endif;
-
-			/* Start the Loop */
-			while ( have_posts() ) :
-				the_post();
-
-				/*
-				 * Include the Post-Type-specific template for the content.
-				 * If you want to override this in a child theme, then include a file
-				 * called content-___.php (where ___ is the Post Type name) and that will be used instead.
-				 */
-				get_template_part( 'template-parts/content', get_post_type() );
-
-			endwhile;
-
-			the_posts_navigation();
-
-		else :
-
-			get_template_part( 'template-parts/content', 'none' );
-
-		endif;
-		?>
+		
+		<?php get_template_part( 'template-parts/footer-callout' ); ?>
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
---
+++ wp-content/themes/alberici-hillsdale/template-parts/footer-callout.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Template part for displaying page content in page.php
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package alberici-hillsdale
+ */
+
+$FooterCalloutHeadline = get_field('footer_callout_header');
+$FooterCalloutContent = get_field('footer_callout_content');
+$FooterCalloutText = get_field('footer_callout_button_text');
+$FooterCalloutLink = get_field('footer_callout_button_link');
+?>
+
+<?php if ($FooterCalloutContent || $FooterCalloutHeadline) : ?>
+<div class="footer-callout container">
+    <h2><?php echo $FooterCalloutHeadline; ?></h2>
+    <div calss="footer-callout-content">
+        <?php echo $FooterCalloutContent; ?>
+    </div>
+
+    <?php if ($FooterCalloutText) : ?>
+    <div class="footer-callout-button">
+        <a href="<?php echo $FooterCalloutLink; ?>" class="btn"><?php echo $FooterCalloutText; ?></a>
+    </div>
+    <?php endif; ?>
+</div>
+<?php endif; ?>
