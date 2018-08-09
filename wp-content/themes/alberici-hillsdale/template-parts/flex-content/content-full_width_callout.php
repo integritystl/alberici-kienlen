fde87a3eecba73bc14dc9d0782df3f9e351eccef
lhornsby <lindsayhornsby@gmail.com>
2018-08-09 15:25:34 -0500

    setup Sass file for Flex Field styles, add some markup for 50-50 flex and full-width flex

--- wp-content/themes/alberici-hillsdale/template-parts/flex-content/content-full_width_callout.php
+++ wp-content/themes/alberici-hillsdale/template-parts/flex-content/content-full_width_callout.php
@@ -4,15 +4,25 @@
 * Component for full-width callout. Includes image, text, button and button link
 *
 **/
+
+$fullWidthImg =  get_sub_field('image');
+$fullWidthHeader =  get_sub_field('header');
+$fullWidthContent =   get_sub_field('content');
+$fullWidthBtnText = get_sub_field('button_text');
+$fullWidthBtnLink =   get_sub_field('button_link');
+
 ?>
 <div class="flex_full_width">
-
-  <?php
-
-  the_sub_field('image');
-  the_sub_field('header');
-  the_sub_field('content');
-  the_sub_field('button_text');
-  the_sub_field('button_link');
-   ?>
+  <div class="container">
+    <div class="full_width_img">
+      <?php echo wp_get_attachment_image($fullWidthImg, 'large');  ?>
+    </div>
+    <div class="full_width_content">
+      <h2><?php echo $fullWidthHeader; ?></h2>
+      <?php echo $fullWidthContent;?>
+       <?php if ($fullWidthBtnText) { ?>
+         <a href="<?php echo $fullWidthBtnLink; ?>"><?php echo $fullWidthBtnText; ?></a>
+       <?php } ?>
+     </div>
+ </div>
 </div>
