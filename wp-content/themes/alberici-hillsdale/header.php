ba63bb089dff0b1b759c9bf8670eda8770ceec6d
laura-khl <laura.kong@integritystl.com>
2018-08-23 14:13:46 -0500

    add back the default site-branding setup

--- wp-content/themes/alberici-hillsdale/header.php
+++ wp-content/themes/alberici-hillsdale/header.php
@@ -40,7 +40,22 @@
 		<div class="main-header">
 			<div class="container">
 				<div class="site-branding">
-					
+						<?php
+						the_custom_logo();
+						if ( is_front_page() && is_home() ) :
+							?>
+							<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+							<?php
+						else :
+							?>
+							<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
+							<?php
+						endif;
+						$alberici_hillsdale_description = get_bloginfo( 'description', 'display' );
+						if ( $alberici_hillsdale_description || is_customize_preview() ) :
+							?>
+							<p class="site-description"><?php echo $alberici_hillsdale_description; /* WPCS: xss ok. */ ?></p>
+						<?php endif; ?>
 				</div><!-- .site-branding -->
 
 				
