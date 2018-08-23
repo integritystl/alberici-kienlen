93ea340be047cdeae9c2ec031ce82d153a83155b
laura-khl <laura.kong@integritystl.com>
2018-08-23 13:32:40 -0500

    Single Post Template. Add share and related articles sidebar

--- wp-content/themes/alberici-hillsdale/single.php
+++ wp-content/themes/alberici-hillsdale/single.php
@@ -10,21 +10,47 @@
 get_header();
 ?>
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main">
+<main id="main" class="site-main">
+	<div class="container">
+		<div id="primary" class="content-area">
+			<p class="breadcrumb"><a class="arrow-link left-arrow" href="<?php echo get_permalink('259'); ?>">Back to News</a></p>
+			<?php
+			if ( have_posts() ) :
+				/* Start the Loop */
+				while ( have_posts() ) :
+					the_post();
 
-		<?php
-		while ( have_posts() ) :
-			the_post();
+					/*
+					* Include the Post-Type-specific template for the content.
+					* If you want to override this in a child theme, then include a file
+					* called content-___.php (where ___ is the Post Type name) and that will be used instead.
+					*/
+					get_template_part( 'template-parts/content', get_post_type() );
 
-			get_template_part( 'template-parts/content', get_post_type() );
+				endwhile;
 
+				the_posts_navigation();
 
-		endwhile; // End of the loop.
-		?>
+			else :
 
-		</main><!-- #main -->
-	</div><!-- #primary -->
+				get_template_part( 'template-parts/content', 'none' );
+
+			endif;
+			?>
+		</div><!-- #primary -->
+
+		<aside id="secondary" class="sidebar-area" role="complementary">
+			<?php include(locate_template('template-parts/share-menu.php')); ?>
+			<div class="news news-latest-3">
+				<div class="container">
+					<p class="headline-lines"><span>Related Articles</span></p>
+					<?php get_template_part( 'template-parts/widget', 'latest-posts' ); ?>
+					<!-- <a href="<?php //echo get_permalink('259'); ?>" class="button all-news">View All News</a> -->
+				</div>
+			</div>
+		</aside>
+	</div>
+</main><!-- #main -->
 
 <?php
 
