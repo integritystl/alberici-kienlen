52535dd8be5cc0f42c888504210e0346ada4acb8
Lindsay <lindsay@integritystl.com>
2018-09-25 10:21:14 -0500

    Start setting up some variables and global styles

--- wp-content/themes/alberici-hillsdale/footer.php
+++ wp-content/themes/alberici-hillsdale/footer.php
@@ -15,7 +15,7 @@
 
 	<footer id="colophon" class="site-footer">
 		<div class="site-info">
-				<div class="container">
+				<div class="container grid-container">
 				<div class="site-copyright">
 					<p class="copyright">Copyright <?php echo date('Y'); ?> <?php the_field('footer_copyright_message', 'option'); ?> | <a href="tel:+1 <?php the_field('footer_phone_number','option');?>"><?php the_field('footer_phone_number','option')?></a></p>
 				</div>
