84473f0d1a140ff82cdcd6fc1c48d7c04eff1246
Lindsay <lindsay@integritystl.com>
2018-09-25 10:26:46 -0500

    adjust parameters for wp_get_attachment_image

--- wp-content/themes/alberici-hillsdale/page-market.php
+++ wp-content/themes/alberici-hillsdale/page-market.php
@@ -12,9 +12,9 @@ get_header();
 
 <div id="primary" class="content-area">
 	<main id="main" class="site-main">
-    
+
     <?php get_template_part( 'template-parts/hero' );?>
-	
+
 
 		<?php if ( get_the_content() ) { ?>
             <div class="entry-content container">
@@ -27,32 +27,32 @@ get_header();
         <?php if( have_rows('market_callouts') ): ?>
             <div class="market-callouts container">
             <?php while ( have_rows('market_callouts') ) : the_row();
-                $market_callout_post = get_sub_field('market_callouts_market'); 
+                $market_callout_post = get_sub_field('market_callouts_market');
                 $market_callout_image = get_sub_field('market_callouts_image');
 
                 if ( $market_callout_post ) : ?>
                     <div class="market-item">
-                        <?php 
+                        <?php
                         $size = 'thumbnail'; // (thumbnail, medium, large, full or custom size)
                         if( $market_callout_image ) {
-                            echo wp_get_attachment_image( $market_callout_image, $size, false, 'market-icon' );
+                            echo wp_get_attachment_image( $market_callout_image, $size );
                         }
-                        if( $market_callout_post ): 
+                        if( $market_callout_post ):
                             // override $post
                             $post = $market_callout_post;
-                            setup_postdata( $post ); 
+                            setup_postdata( $post );
                             ?>
                             <h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
                             <?php wp_reset_postdata(); // IMPORTANT - reset the $post object so the rest of the page works correctly ?>
                         <?php endif;?>
-                    </div> 
-                <?php endif; 
+                    </div>
+                <?php endif;
             endwhile; ?>
             </div>
         <?php endif;?>
 
         <h3 class="container">Services</h3>
-        
+
         <?php
 			if(have_rows('flexible_content', get_the_ID())):
 				while(have_rows('flexible_content')): the_row();
@@ -62,7 +62,7 @@ get_header();
         ?>
 
         <?php get_template_part( 'template-parts/footer-callout' );?>
-    
+
     </main><!-- #main -->
 </div><!-- #primary -->
 
