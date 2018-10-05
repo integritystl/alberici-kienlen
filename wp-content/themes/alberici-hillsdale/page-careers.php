de887a7fc1d121ca6b47b6c3ba98ea169e455b64
laura-khl <laura.kong@integritystl.com>
2018-10-05 16:27:16 -0500

    career page styling

--- wp-content/themes/alberici-hillsdale/page-careers.php
+++ wp-content/themes/alberici-hillsdale/page-careers.php
@@ -15,35 +15,39 @@ $jobsCalloutgform = get_field('jobs_callout_careers_form');
 ?>
 <div id="primary" class="content-area">
 	<main id="main" class="site-main">
-
-    <?php get_template_part( 'template-parts/hero' );?>
-	
-
-		<?php if ( get_the_content() ) { ?>
-            <div class="entry-content container">
-                <?php
-                the_content();
-                ?>
-            </div><!-- .entry-content -->
-        <?php } ?>
-
-        <?php
-			if(have_rows('flexible_content', get_the_ID())):
-				while(have_rows('flexible_content')): the_row();
-					include(locate_template('template-parts/flex-content/content-' . get_row_layout() . '.php'));
-				endwhile;
-			endif;
-        ?>
+        <div class="career-template">
+            <?php get_template_part( 'template-parts/hero' );?>
         
-        <div class="job-callout container">
-            <h3><?php echo $jobsCalloutHeader; ?></h3>
-            <p><?php echo $jobsCalloutContent; ?></p>
-            <?php if ($jobsCalloutLink): ?>
-                <a href="<?php echo $jobsCalloutLink; ?>">VIEW JOB POSTS</a>
-            <?php endif;?>
-            <?php echo $jobsCalloutgform; ?>
+            <?php if ( get_the_content() ) { ?>
+                <div class="career-content container">
+                    <?php
+                    the_content();
+                    ?>
+                </div><!-- .career-content -->
+            <?php } ?>
+
+            <?php
+                if(have_rows('flexible_content', get_the_ID())):
+                    while(have_rows('flexible_content')): the_row();
+                        include(locate_template('template-parts/flex-content/content-' . get_row_layout() . '.php'));
+                    endwhile;
+                endif;
+            ?>
+            
+            <h2 class="headline-lines container"></h2>
+
+            <div class="job-callout container">
+                <div class="callout-content">
+                    <h2><?php echo $jobsCalloutHeader; ?></h2>
+                    <p><?php echo $jobsCalloutContent; ?></p>
+                    <?php if ($jobsCalloutLink): ?>
+                        <a href="<?php echo $jobsCalloutLink; ?>">VIEW JOB POSTS</a>
+                    <?php endif;?>
+                </div>
+                <?php echo $jobsCalloutgform; ?>
+            </div>
+            <?php get_template_part( 'template-parts/footer-callout' );?>
         </div>
-        <?php get_template_part( 'template-parts/footer-callout' );?>
 
         
 	</main><!-- #main -->
