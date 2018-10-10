e9ef7998c2af726d6e83e5731dd1b8c3179d7016
laura-khl <laura.kong@integritystl.com>
2018-10-10 12:53:41 -0500

    add single project page style

--- wp-content/themes/alberici-hillsdale/single-project.php
+++ wp-content/themes/alberici-hillsdale/single-project.php
@@ -13,64 +13,63 @@ $client = get_field('project_client');
 $owner = get_field('project_owner');
 $project_size = get_field('project_size');
 ?>
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main">
-			<div class="container">
-			<?php
-			if ( have_posts() ) :
-				while ( have_posts() ) : 
-					the_post();?>
-					<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-					<div class="hero-area" style="background-image: url(<?php if ($HeroImage): echo $HeroImage; endif; ?>);">
-                        <h2 class="entry-title"><?php the_title(); ?></h2>
-                        <p class="project-location"></p> <!--leave it until the location taxonomy is ready-->
-                        
-                        <a href="">TRANSPORTATION</a> <!--fillup the url later， need to find where it links to. -->
-                        <div class="project-info">
-                            <p class="project-client"><?php echo $client; ?></p>
-                            <p class="project-owner"><?php echo $owner; ?></p>
-                            <p class="project-size"><?php echo $project_size; ?></p>
-                        </div>
-					</div><!-- .hero-area -->
+
+<div id="primary" class="content-area">
+	<main id="main" class="site-main">
+		<?php
+		if ( have_posts() ) :
+			while ( have_posts() ) :
+				the_post();?>
+				<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+					<div class="single-project-hero" style="background-image: url(<?php if ($HeroImage): echo $HeroImage; endif; ?>);">
+						<div class="headline">
+							<h1 class="entry-title"><?php the_title(); ?></h1>
+							<p class="project-location">Chicago, Illinois</p> <!--leave it until the location taxonomy is ready-->
+							<div class="market-link">
+							<a href="">Transportation</a> <!--fillup the url later， need to find where it links to. -->
+							</div>
+						</div>
+						<div class="project-info">
+							<div class="container">
+							<p class="project-client"><?php echo $client; ?></p>
+							<p class="project-owner"><?php echo $owner; ?></p>
+							<p class="project-size"><?php echo $project_size; ?></p>
+							</div>
+						</div>
+					</div><!-- .single-project-hero -->
 					<nav class="breadcrumbs container" aria-label="Breadcrumb navigation">
 						<?php
 						if ( function_exists('yoast_breadcrumb') ) {
 							yoast_breadcrumb( '
 							<p id="breadcrumbs">','</p>
 							' );
-						}
-						?>
-                    </nav>
+						} ?>
+					</nav>
 
-                    <div class="entry-content">
-						<?php the_content(); ?>
-                    </div><!-- .entry-content -->
-                    
-                    <?php alberici_hillsdale_post_thumbnail(); ?>
-                    <h5>Services</h5>
-                    <?php 
-                    
-                    if( have_rows('project_services') ):
-                       while ( have_rows('project_services') ) : the_row(); 
-                            $link = get_sub_field('service_link');?>
-                           <a href="<?php echo $link['url']; ?>"> <?php the_sub_field('service_name'); ?></a>
-                       <?php endwhile;                   
-                   endif;
-                    ?>
+					<div class="single-project">
+						<div class="single-project-content">
+							<?php the_content(); ?>
+							<div class="service-list">
+								<h3>Services</h3>
+								<?php
+								if( have_rows('project_services') ):
+									while ( have_rows('project_services') ) : the_row();
+									$link = get_sub_field('service_link');?>
+									<a href="<?php echo $link['url']; ?>"> <?php the_sub_field('service_name'); ?></a>
+									<?php endwhile;
+								endif; ?>
+							</div>
+						</div><!-- .single-project-content -->
+					</div>
 				</article><!-- #post-<?php the_ID(); ?> -->
-				
+			<?php endwhile;
+		else :
+			get_template_part( 'template-parts/content', 'none' );
+		endif; ?>
 
-				<?php endwhile;
-			else :
-				get_template_part( 'template-parts/content', 'none' );
-			endif;
-			?>
-		
-			
-			 <?php get_template_part( 'template-parts/footer-callout' ); ?>
-			<div><!-- container -->
-		</main><!-- #main -->
-	</div><!-- #primary -->
+		<?php get_template_part( 'template-parts/footer-callout' ); ?>
+	</main><!-- #main -->
+</div><!-- #primary -->
 
 <?php
 
