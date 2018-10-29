3377dfb15a29f725a7703341e2aa15d05bf7664a
laura-khl <laura.kong@integritystl.com>
2018-10-29 15:15:39 -0500

    Change news section styling

--- wp-content/themes/alberici-hillsdale/front-page.php
+++ wp-content/themes/alberici-hillsdale/front-page.php
@@ -57,20 +57,22 @@ $HeroButtonLink = get_field('homepage_hero_button_link');
 					<ul class="blog-content_posts">
 					<?php while ($the_query -> have_posts()) : $the_query -> the_post(); ?>
 						<li>
+							<div class="taxonomies">
+								<?php
+								$market_taxonomy = get_the_terms( get_the_ID(), 'market_category' );
+								$service_taxonomy = get_the_terms( get_the_ID(), 'service_category' );
+								if ($market_taxonomy) { ?>
+									<p><?php echo $market_taxonomy[0]->name;?></p>
+								<?php }
+								if ($service_taxonomy) { ?>
+									<p><?php echo $service_taxonomy[0]->name;?></p>
+								<?php } ?>
+							</div>
 							<a href="<?php the_permalink(); ?>">
 								<span class="news-meta">
 									<span class="card-overlay"></span>
 									<?php echo wp_get_attachment_image( get_post_thumbnail_id(get_the_ID()), 'blog_image', false );?>
 									<span class="news-text">
-										<?php
-										$market_taxonomy = get_the_terms( get_the_ID(), 'market_category' );
-										$service_taxonomy = get_the_terms( get_the_ID(), 'service_category' );
-										if ($market_taxonomy) { ?>
-											<p><?php echo $market_taxonomy[0]->name;?></p>
-										<?php }
-										if ($service_taxonomy) { ?>
-											<p><?php echo $service_taxonomy[0]->name;?></p>
-										<?php } ?>
 										<h3><?php  the_title();?></h3>
 									</span>
 								</span>
