04693d6d7e577be0c667888d1297488089206174
laura-khl <laura.kong@integritystl.com>
2018-10-31 13:42:16 -0500

    add styling to single post page

--- wp-content/themes/alberici-hillsdale/template-parts/content.php
+++ wp-content/themes/alberici-hillsdale/template-parts/content.php
@@ -12,18 +12,36 @@
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<header class="entry-header">
 		<?php
+		$market_taxonomy = get_the_terms( get_the_ID(), 'market_category' );
+		$service_taxonomy = get_the_terms( get_the_ID(), 'service_category' );
+
 		if ( is_singular() && 'post' === get_post_type() ) :
 			the_title( '<h1 class="entry-title">', '</h1>' ); ?>
 			<div class="entry-meta">
 				<p class="post-date">
 					<?php alberici_hillsdale_posted_on(); ?>
-					<span classs="post-category"><?php echo get_the_category_list(' | '); ?></span>
+					<span class="post-category">
+						<?php if ($market_taxonomy) { ?>
+							<span><?php echo $market_taxonomy[0]->name;?></span>
+						<?php }
+						if ($service_taxonomy) { ?>
+							<span><?php echo $service_taxonomy[0]->name;?></span>
+						<?php } ?>
+						<?php echo get_the_category_list(' | '); ?>
+					</span>
 				</p>
 			</div>
 		<?php 
 		else :
 			the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' ); ?>
-			<span class="post-category"><?php echo get_the_category_list(' | '); ?></span>
+			<span class="post-category"><?php echo get_the_category_list(' | '); ?>
+				<?php if ($market_taxonomy) { ?>
+				<p><?php echo $market_taxonomy[0]->name;?></p>
+			<?php }
+			if ($service_taxonomy) { ?>
+				<p><?php echo $service_taxonomy[0]->name;?></p>
+			<?php } ?>
+			</span>
 			<span class="post-date">Posted on <?php alberici_hillsdale_posted_on(); ?></span>
 		<?php endif; ?>
 	</header><!-- .entry-header -->
