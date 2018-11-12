55be134c1f265bceadc7ff905579691242ef7e66
laura-khl <laura.kong@integritystl.com>
2018-11-12 13:42:36 -0600

    404 page styling

--- wp-content/themes/alberici-hillsdale/404.php
+++ wp-content/themes/alberici-hillsdale/404.php
@@ -8,14 +8,23 @@
  */
 
 get_header();
+
+$heroImage = get_field('404_hero_featured_image', 'option');
+$heroHeadline = get_field('404_hero_title', 'option');
 ?>
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main">
 
 			<section class="error-404 not-found">
+				<?php if ($heroHeadline): ?>
+					<div class="hero-area" style="background-image: url( <?php if ($heroImage): echo $heroImage; endif; ?> );">
+							<h1><?php echo $heroHeadline; ?></h1>
+							<div class="hero-overlay"></div>
+					</div>
+				<?php endif; ?>
 
-				<div class="page-content">
+				<div class="page-content container">
 					<?php the_field('404_page','option'); ?>
 				</div><!-- .page-content -->
 			</section><!-- .error-404 -->
