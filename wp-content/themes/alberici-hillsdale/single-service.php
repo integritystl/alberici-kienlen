953e3a2efe6260b062510f81d8f0555d80d9cb3b
laura-khl <laura.kong@integritystl.com>
2018-11-20 13:10:40 -0600

    change related projects sorted by service

--- wp-content/themes/alberici-hillsdale/single-service.php
+++ wp-content/themes/alberici-hillsdale/single-service.php
@@ -35,42 +35,45 @@ $HeroImage = get_field('general_hero_featured_image');
 				get_template_part( 'template-parts/content', 'none' );
 			endif;
 
-			// Show Latest 3 Projects
-			$latest_args = array(
-			'post_type' =>  'project',
-			'posts_per_page' => 3,
-			'post_status' => 'publish'
-			);
-			$latest_query = new WP_Query( $latest_args );
-
+			
+			if ( is_single() && 'service' === get_post_type()) {
+				$term = get_post()->post_name;
+				$latest_args = array(
+					'post_type' => 'project',
+					'posts_per_page' => 3,
+					'post_status' => 'publish',
+					'post__not_in' => array( get_the_ID() ),
+					'orderby' => 'rand',
+					'tax_query' => array(
+						array(
+							'taxonomy' => 'service_category',
+							'field' => 'slug',
+							'terms' => $term
+						)
+					)
+				);
+				$latest_query = new WP_Query( $latest_args );
+			}
 
 			if ( $latest_query->have_posts() ) : ?>
 				<div class="services services-latest-3 container">
 					<h2>Related Projects</h2>
-					<ul>
+					<div class="card-group blog-content_posts">
 					<?php while( $latest_query->have_posts() ) : $latest_query->the_post();?>
-					<li>
-						<a href="<?php the_permalink(); ?>">
-							<span class="card-overlay"></span>
+						<article class="card-post card-news post">
+							<div class="card-overlay"></div>
 							<?php echo wp_get_attachment_image( get_post_thumbnail_id(get_the_ID()), 'blog_image', false );?>
-							<span class="projects-text">
-								<?php
-								$market_taxonomy = get_the_terms( get_the_ID(), 'market_category' );
-								$service_taxonomy = get_the_terms( get_the_ID(), 'service_category' );
-								if ($market_taxonomy) { ?>
-									<p><?php echo $market_taxonomy[0]->name;?></p>
-								<?php }
-								if ($service_taxonomy) { ?>
-									<p><?php echo $service_taxonomy[0]->name;?></p>
-								<?php } ?>
-								<h3><?php  the_title();?></h3>
-							</span>
-						</a>
-					</li>
-					<?php endwhile; ?>
-					</ul>
+							<a href="<?php the_permalink(); ?>">
+								<div class="news-meta">
+									<h3><?php  the_title();?></h3>
+								</div>
+							</a>
+						</article>
+						<?php endwhile; ?>
+					</div>
+					<a href="<?php echo get_home_url(); ?>/projects" class="btn"> view more projects </a>
 				<?php endif; wp_reset_postdata(); ?>
-				<a href="<?php echo get_home_url(); ?>/projects" class="btn"> view more projects </a>
+				
 			</div>
 			<?php get_template_part( 'template-parts/footer-callout' ); ?>
 		</main><!-- #main -->
