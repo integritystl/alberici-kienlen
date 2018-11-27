9714139b380fe1b1169efd9990d8a4e4c2d6f096
Lindsay <lindsay@integritystl.com>
2018-11-27 15:34:14 -0600

    Add ACF field to select the project Service Taxonomy that goes with a Service Post

--- wp-content/themes/alberici-hillsdale/theme_infrastructure/CustomPostTypes/ServiceCPT.php
+++ wp-content/themes/alberici-hillsdale/theme_infrastructure/CustomPostTypes/ServiceCPT.php
@@ -31,7 +31,55 @@ class ServiceCPT
 
     public static function addServiceACF()
     {
+      if( function_exists('acf_add_local_field_group') ):
 
+        acf_add_local_field_group(array(
+        	'key' => 'group_5bfdac6de4418',
+        	'title' => 'Project Service Category',
+        	'fields' => array(
+        		array(
+        			'key' => 'field_5bfdac8e87d03',
+        			'label' => 'Project Service Category',
+        			'name' => 'project_service_category',
+        			'type' => 'taxonomy',
+        			'instructions' => 'Assign the appropriate Project Service category for this Service.',
+        			'required' => 0,
+        			'conditional_logic' => 0,
+        			'wrapper' => array(
+        				'width' => '',
+        				'class' => '',
+        				'id' => '',
+        			),
+        			'taxonomy' => 'service_category',
+        			'field_type' => 'select',
+        			'allow_null' => 0,
+        			'add_term' => 0,
+        			'save_terms' => 0,
+        			'load_terms' => 0,
+        			'return_format' => 'id',
+        			'multiple' => 0,
+        		),
+        	),
+        	'location' => array(
+        		array(
+        			array(
+        				'param' => 'post_type',
+        				'operator' => '==',
+        				'value' => 'service',
+        			),
+        		),
+        	),
+        	'menu_order' => 0,
+        	'position' => 'side',
+        	'style' => 'default',
+        	'label_placement' => 'top',
+        	'instruction_placement' => 'label',
+        	'hide_on_screen' => '',
+        	'active' => 1,
+        	'description' => '',
+        ));
+
+        endif;
     }
 }
 
