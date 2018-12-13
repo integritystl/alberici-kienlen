1be564b8d3731bea9dd10903e0d48afcbcfa6e16
Lindsay <lindsay@integritystl.com>
2018-12-13 10:29:55 -0600

    Add Production deploy script for Kienlen

---
+++ scripts/deploy-production-kienlen.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Fail the build on any failed command
+set -e
+mkdir -p $HOME/.ssh
+git config --global user.email "dev-team@integritystl.com"
+git config --global user.name "Automated Deployment"
+ssh-keyscan -H 45.79.171.150 >> $HOME/.ssh/known_hosts
+ssh-keyscan -H git.wpengine.com >> $HOME/.ssh/known_hosts
+echo -e $PRIVATE_SSH_KEY >> $HOME/.ssh/id_rsa
+chmod 600 $HOME/.ssh/id_rsa
+ssh -vvv git@git.wpengine.com info
+git remote add production git@git.wpengine.com:production/kienlen.git
+git push production master
