693dc34b8e40d2dab727dfcf5e5cc6d99aeab708
Jordan Fautley <>
2019-02-25 16:07:31 -0500

    php error fix 4

--- wp-content/themes/alberici-hillsdale/single-market.php
+++ wp-content/themes/alberici-hillsdale/single-market.php
@@ -66,10 +66,46 @@ $HeroImage = get_field('general_hero_featured_image');
 			);
 			$latest_query = new WP_Query( $latest_args );
 
-		endif;
+		endif; ?>
 
-		if ( $latest_query->have_posts() ) : ?>
+		<?php if ( $latest_query->have_posts() ) : ?>
+			<div class="container">
+				<div class="markets markets-latest-3">
+					<h2>Related Projects</h2>
+					<ul>
+						<?php while( $latest_query->have_posts() ) : $latest_query->the_post();
+							$categories = get_the_category();
+							$category_names = '';
 
+							if ( ! empty( $categories ) ) {
+								foreach($categories as $category) {
+									$category_names .= $category->name . ' ';
+								}
+							} ?>
+							<li>
+								<p><?php $category = get_the_category(); echo $category[0]->cat_name; ?></p>
+								<a href="<?php the_permalink(); ?>">
+									<span class="card-overlay"></span>
+									<?php echo wp_get_attachment_image( get_post_thumbnail_id(get_the_ID()), 'blog_image', false );?>
+									<span class="projects-text">
+										<?php
+										$market_taxonomy = get_the_terms( get_the_ID(), 'market_category' );
+										$service_taxonomy = get_the_terms( get_the_ID(), 'service_category' );
+										if ($market_taxonomy) { ?>
+											<p><?php echo $market_taxonomy[0]->name;?></p>
+										<?php }
+										if ($service_taxonomy) { ?>
+											<p><?php echo $service_taxonomy[0]->name;?></p>
+										<?php } ?>
+										<h3><?php  the_title();?></h3>
+									</span>
+								</a>
+							</li>
+						<?php endwhile; ?>
+					</ul>
+					<a href="<?php echo get_home_url(); ?>/projects" class="btn"> VIEW PROJECTS </a>
+				</div>
+			</div>
 		<?php endif; wp_reset_postdata(); ?>
 
 
