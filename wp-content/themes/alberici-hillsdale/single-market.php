272d5618351fff15773533b88686970075c41579
Jordan Fautley <>
2019-02-25 15:41:23 -0500

    php error take 3

--- wp-content/themes/alberici-hillsdale/single-market.php
+++ wp-content/themes/alberici-hillsdale/single-market.php
@@ -36,10 +36,46 @@ $HeroImage = get_field('general_hero_featured_image');
 			get_template_part( 'template-parts/content', 'none' );
 		endif; ?>
 
+		<?php if ( is_single() && 'market' === get_post_type()) :
+			$terms = get_the_terms( get_the_ID(), 'category' );
+			$term_list = wp_list_pluck( $terms, 'slug' );
+			$latest_args = array(
+				'post_type' => 'market',
+				'posts_per_page' => 3,
+				'post_status' => 'publish',
+				'post__not_in' => array( get_the_ID() ),
+				'orderby' => 'rand',
+				'tax_query' => array(
+					array(
+						'taxonomy' => 'category',
+						'field' => 'slug',
+						'terms' => $term_list
+					)
+				)
+			);
+			$latest_query = new WP_Query( $latest_args );
+
+		endif;
+
+		//Default: Show Latest 3 Posts
+		if(!$latest_query->have_posts()) :
+			$latest_args = array(
+			'post_type' =>  'project',
+			'posts_per_page' => 3,
+			'post_status' => 'publish'
+			);
+			$latest_query = new WP_Query( $latest_args );
+
+		endif;
+
+		if ( $latest_query->have_posts() ) : ?>
+
+		<?php endif; wp_reset_postdata(); ?>
 
 
 
-			<?php get_template_part( 'template-parts/footer-callout' ); ?>
+
+		<?php get_template_part( 'template-parts/footer-callout' ); ?>
 	</main><!-- #main -->
 </div><!-- #primary -->
 
