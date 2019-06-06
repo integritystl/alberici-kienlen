cebc3ddbd532d17423805baa60520f00e2ca4f9c
Jordan Fautley <>
2019-06-06 18:17:46 -0400

    removing GA for now

--- wp-content/themes/alberici-hillsdale/header.php
+++ wp-content/themes/alberici-hillsdale/header.php
@@ -18,15 +18,6 @@
 
  	<?php wp_head(); ?>
 
-    <!-- Global site tag (gtag.js) - Google Analytics -->
-  <script async src="https://www.googletagmanager.com/gtag/js?id=UA-25892144-2"></script>
-  <script>
-   window.dataLayer = window.dataLayer || [];
-   function gtag(){dataLayer.push(arguments);}
-   gtag('js', new Date());
-
-   gtag('config', 'UA-25892144-2');
-  </script>
  </head>
 
 <?php $theme_config = get_field('set_site', 'options');?>
