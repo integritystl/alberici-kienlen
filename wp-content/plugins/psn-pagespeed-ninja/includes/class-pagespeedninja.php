e0cad886d0b7136b2c5883c68e3e1ff5688872af
Julia Cramer <julia.cramer@integritystl.com>
2020-01-06 11:01:02 -0600

    Update PageSpeed Ninja

--- wp-content/plugins/psn-pagespeed-ninja/includes/class-pagespeedninja.php
+++ wp-content/plugins/psn-pagespeed-ninja/includes/class-pagespeedninja.php
@@ -20,7 +20,7 @@ class PagespeedNinja
     public function __construct() {
         $this->plugin_name     = 'pagespeedninja';
         $this->plugin_slug     = 'psn-pagespeed-ninja';
-        $this->version         = '0.9.39';
+        $this->version         = '0.9.40';
         $this->plugin_dir_path = plugin_dir_path(dirname(__FILE__));
     }
 
